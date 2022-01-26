060be4e759e8217ecaa9152baa735e15a258391e
ryuinw123 <romtham.thang@gmail.com>
2022-01-26 17:07:15 +0700

    Ok

--- portfolio/src/components/ProductData.js
+++ portfolio/src/components/ProductData.js
@@ -33,32 +33,32 @@ export const ProductData2 = [
     {
         id: 1,
         img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242075983922/Coursera_23MFAD46U4NJ-1.jpg",
-        link: "coursera.org/verify/23MFAD46U4NJ"
+        link: "https://coursera.org/verify/23MFAD46U4NJ"
 
     },
     {
         id: 2,
         img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242491203604/Coursera_DDEZ3WG6EFRT_2-1.jpg",
-        link: "coursera.org/verify/DDEZ3WG6EFRT"
+        link: "https://coursera.org/verify/DDEZ3WG6EFRT"
     },
     {
         id: 3,
         img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242784837672/Coursera_J8NHX5KG23HL-1.jpg",
-        link: "coursera.org/verify/J8NHX5KG23HL"
+        link: "https://coursera.org/verify/J8NHX5KG23HL"
     },
     {
         id: 4,
         img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243019698186/Coursera_MRSG2EAJNZLW-1.jpg",
-        link: "coursera.org/verify/MRSG2EAJNZLW"
+        link: "https://coursera.org/verify/MRSG2EAJNZLW"
     },
     {
         id: 5,
         img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243346866176/Coursera_NHGVS6RR6RRS-1.jpg",
-        link: "coursera.org/verify/NHGVS6RR6RRS"
+        link: "https://coursera.org/verify/NHGVS6RR6RRS"
     },
     {
         id: 6,
         img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243590131742/Coursera_PNGM8TTSCY8F-1.jpg",
-        link: "coursera.org/verify/PNGM8TTSCY8F"
+        link: "https://coursera.org/verify/PNGM8TTSCY8F"
     }
 ]
