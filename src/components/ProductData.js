cfe663e8e216862457947f9c8f52523ce7cf060c
ryuinw123 <47142982+ryuinw123@users.noreply.github.com>
2025-01-21 21:26:07 +0700

    Update ProductData.js

--- src/components/ProductData.js
+++ src/components/ProductData.js
@@ -2,13 +2,13 @@
 export const ProductData1 = [
     {
         id: 1,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1152961713759588533/Design-Patterns-in-Software-Development.png",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1152961713759588533/Design-Patterns-in-Software-Development.png?ex=6790f4c5&is=678fa345&hm=399db7cb98aa98fd06a007ae44bb518334fe19562ade5bca01b0da930915e627&",
         link: "https://github.com/ryuinw123/SOFTWARE-ARCHITECTURE-AND-DESIGN"
 
     },
     {
         id: 2,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935196129811906560/PacketTracer.PNG",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935196129811906560/PacketTracer.PNG?ex=67906784&is=678f1604&hm=f2cda5d106c1ec2b538d1400aaf708f59da31c5363c8b824ca9cd7eedf08505b&",
         link: "https://github.com/ryuinw123/INTERNETWORKING-STANDARDS-AND-TECHNOLOGIES"
     },
     {
@@ -18,33 +18,28 @@ export const ProductData1 = [
     },
     {
         id : 4,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935971787915800576/FireShot_Capture_001_-_React_App_-_localhost.png",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935971787915800576/FireShot_Capture_001_-_React_App_-_localhost.png?ex=679096e7&is=678f4567&hm=46a5dc44d4d2a5ace5d164326a84264612d97b689e11be92df6891b249d25125&",
         link: "https://github.com/ryuinw123/React-Portfolio"
     },
     {
         id : 5,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1005812406208958474/Capture.PNG",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1005812406208958474/Capture.PNG?ex=6790e24c&is=678f90cc&hm=13af07791fbbbd19e06d6de8f2c32cb2b7240c28a28de5f7bc4eee280a78b058&",
         link: "https://github.com/ryuinw123/ImageClassificationSourceCode"
     },
     {
         id : 6,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1005812406691315804/Capture2.PNG",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1005812406691315804/Capture2.PNG?ex=6790e24c&is=678f90cc&hm=fc372b72b777f9c7c35a2c3884bf20c65ec2f8bfa780041238df7aa0cbd7317c&",
         link: "https://github.com/ryuinw123/CurrencyExchange"
     },
     {
         id : 7,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1102965720213635152/image.png",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1102965720213635152/image.png?ex=6790589f&is=678f071f&hm=30738cf46c58d731f2bbaf45b598d2887f0888e54814edcdc318a513150dcf32&",
         link: "https://github.com/ryuinw123/kotlincompanion"
     },
     {
         id : 8,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1152961562760462429/image.png",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1152961562760462429/image.png?ex=6790f4a1&is=678fa321&hm=6775c8ca132fc1556974be810a9200997dce89b1046784a4e5cbcb2041fd6d04&",
         link: "https://github.com/ryuinw123/react-admin-companion"
-    },
-    {
-        id : 9,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1152961562760462429/image.png",
-        link: "https://github.com/ryuinw123/admincompanion"
     },
 ]
 
@@ -52,97 +47,102 @@ export const ProductData1 = [
 export const ProductData2 = [
     {
         id: 1,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242075983922/Coursera_23MFAD46U4NJ-1.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242075983922/Coursera_23MFAD46U4NJ-1.jpg?ex=6790697c&is=678f17fc&hm=70ec41d0792f7b6ee0869fcfe00e65c272852a595ff837d949a350e68fedeeca&",
         link: "https://coursera.org/verify/23MFAD46U4NJ"
 
     },
     {
         id: 2,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242491203604/Coursera_DDEZ3WG6EFRT_2-1.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242491203604/Coursera_DDEZ3WG6EFRT_2-1.jpg?ex=6790697c&is=678f17fc&hm=dc254e453878f7f42d1edaae8a7a720efe1418347cc5b4281c10bc16dc2af223&",
         link: "https://coursera.org/verify/DDEZ3WG6EFRT"
     },
     {
         id: 3,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242784837672/Coursera_J8NHX5KG23HL-1.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198242784837672/Coursera_J8NHX5KG23HL-1.jpg?ex=6790697c&is=678f17fc&hm=8dd847489d261fa2582ef713bb9e2be71ffc243257f5f19b37f2e9ab5be0e7e6&",
         link: "https://coursera.org/verify/J8NHX5KG23HL"
     },
     {
         id: 4,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243019698186/Coursera_MRSG2EAJNZLW-1.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243019698186/Coursera_MRSG2EAJNZLW-1.jpg?ex=6790697c&is=678f17fc&hm=c4019302678a2b2d73738e38f217f952b5adc29cfc52f8124dfb35e7c9a941e3&",
         link: "https://coursera.org/verify/MRSG2EAJNZLW"
     },
     {
         id: 5,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243346866176/Coursera_NHGVS6RR6RRS-1.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243346866176/Coursera_NHGVS6RR6RRS-1.jpg?ex=6790697c&is=678f17fc&hm=94885fc8182c0722d2ff6bfaa22a651ebfaa78ae6cd86133f87f4b7fef31f965&",
         link: "https://coursera.org/verify/NHGVS6RR6RRS"
     },
     {
         id: 6,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243590131742/Coursera_PNGM8TTSCY8F-1.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935198243590131742/Coursera_PNGM8TTSCY8F-1.jpg?ex=6790697c&is=678f17fc&hm=352c59405a586eb8f3601bad432109815a4977fda34ab6e070802de70992503d&",
         link: "https://coursera.org/verify/PNGM8TTSCY8F"
     },
     {
         id: 7,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1005808178467962890/Coursera_6BNPQU58KLPX-1.png",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1005808178467962890/Coursera_6BNPQU58KLPX-1.png?ex=6790de5c&is=678f8cdc&hm=de841be805d65162a3a99253c38df64632edd5a74e05bab10e9a0e4a6886a736&",
         link: "https://coursera.org/verify/6BNPQU58KLPX"
     },
     {
         id: 8,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1152961006675427399/Coursera_MJKAFVC9W2JH-1.png",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1152961006675427399/Coursera_MJKAFVC9W2JH-1.png?ex=6790f41c&is=678fa29c&hm=a2cc446a32b2f8996049d23d921368e65ab2458be8a0815b6f5528ec81faa901&",
         link: "https://coursera.org/verify/MJKAFVC9W2JH"
     },
     {
         id: 8,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/1010652401902026802/Certificate_of_Achievement_-_Mr._Romtham_Tungsununtham_1.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1010652401902026802/Certificate_of_Achievement_-_Mr._Romtham_Tungsununtham_1.jpg?ex=6790b1a5&is=678f6025&hm=821f597419365242e7be682acd623d84292cc4fcacec8af4fb4c2d4334d697c1&",
         link: "/"
+    },
+    {
+        id: 9,
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/1331267771341738027/image.png?ex=6790ff3a&is=678fadba&hm=0c148f81fece5dd3094c21ab6e05cef75678b6304a8c5e8b53acae4e1ac5036c&",
+        link: "https://coursera.org/share/25fbc3157d114b489574a3d624125078"
     },
 ]
 
 export const ProductData3 = [
     {
         id: 1,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935963060550660116/IMG_3168.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935963060550660116/IMG_3168.jpg?ex=67908ec6&is=678f3d46&hm=ae04a9406f3d73db2f9c69236631d9eb2b1478b1a53b40269580309f8a450249&",
         link: "/"
 
     },
     {
         id: 2,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935963065529278514/IMG_3169.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935963065529278514/IMG_3169.jpg?ex=67908ec8&is=678f3d48&hm=1d91037fc502042648dee7b448e0f6355580e2970a88004dbb33233d5c6480c5&",
         link: "/"
     },
     {
         id: 3,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935963070000410634/IMG_3170.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935963070000410634/IMG_3170.jpg?ex=67908ec9&is=678f3d49&hm=8487bf15ff78331b5c15373e218efebce3007b6d760966256d913f4a6de533a1&",
         link: "/"
     },
     {
         id: 4,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935963084521095238/IMG_3171.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935963084521095238/IMG_3171.jpg?ex=67908ecc&is=678f3d4c&hm=d8f60db546704b9885ffba3a8077f6a1f14390bfedce407c06ee49cb44c63853&",
         link: "/"
     },
     {
         id: 5,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964772288053328/IMG_3172.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964772288053328/IMG_3172.jpg?ex=6790905f&is=678f3edf&hm=f2c5e1c526aa276419b57320ef3466766c6a40c3a3acf2a2f56521ef4a5c263d&",
         link: "/"
     },
     {
         id: 6,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964772728467456/IMG_3173.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964772728467456/IMG_3173.jpg?ex=6790905f&is=678f3edf&hm=a8963237bd2bbfba9f7605d8fd70c4c2202b539eb8434d049aca1a7164c72c0c&",
         link: "/"
     },
     {
         id: 7,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964773152067644/IMG_3174.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964773152067644/IMG_3174.jpg?ex=6790905f&is=678f3edf&hm=7cc912eb39c29ff9a28f7e3cae526618f5e451ce9532bb92ed0bfc6afde61720&",
         link: "/"
     },
     {
         id: 8,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964773575696384/IMG_3175.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964773575696384/IMG_3175.jpg?ex=6790905f&is=678f3edf&hm=a398b82cf5221254521a7c2c19dc7d5211e74c900acf0eeafbbfa12e53809e9d&",
         link: "/"
     },
     {
         id: 9,
-        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964774456492113/IMG_3176.jpg",
+        img: "https://cdn.discordapp.com/attachments/798836385019723796/935964774456492113/IMG_3176.jpg?ex=6790905f&is=678f3edf&hm=b91a947710c701169462a19e8cec04e0575bf0887e17f1da412271def05809b2&",
         link: "/"
     }
 ]
